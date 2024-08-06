d949166e6e0329518b89dd20fd1e99787b0eb43f
Axel <axel.ein@gmail.com>
2024-08-06 15:35:44 +0200

    [SERVER] changed cors method

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { ENV } from './config.js';
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
+// import cors from 'cors';
 
 // Importation des routes
 import routeurUtilisateur from './src/routes/utilisateur.route.js';
@@ -17,7 +17,27 @@ const app = express();
 // Implémentation des middlewares
 app.use(express.json()); // Pour lire les données JSON
 app.use(cookieParser()); // Pour parser les cookies
-app.use(cors()); // Pour gérer la protection des CORS
+// app.use(cors({
+//     origin: "*"
+// })); // Pour gérer la protection des CORS
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    return next();
+});
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Quelque chose s'est mal passée !";
+    return res.status(status).json({
+        success: false,
+        status,
+        message,
+    });
+});
 
 // Middleware to route
 app.use("/api/utilisateur", routeurUtilisateur);

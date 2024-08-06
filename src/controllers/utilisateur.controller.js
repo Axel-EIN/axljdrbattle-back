e083b78d28d43b5b7796d84167017b4bf52a32d8
Axel <axel.ein@gmail.com>
2024-08-06 17:45:41 +0200

    [USER controller] reformat and comments

--- src/controllers/utilisateur.controller.js
+++ src/controllers/utilisateur.controller.js
@@ -1,116 +1,93 @@
-// Importation de l'Objet Utilisateur initialisé avec la base de données Sequelize
-import { Utilisateur } from "../models/index.js";
+import { Utilisateur } from "../models/index.js"; // Importation du Modèle Utilisateur initialisé connecté à la base données Sequelize
+import bcrypt from "bcrypt"; // Importation de la bibliothèque bcrypt pour crypter le mot de passe
+import jwt from "jsonwebtoken"; // Importation de la bibliothèque jwt pour créer le cookie/token
+import { ENV } from "./../../config.js";
 
-// Importation de la bibliothèque bcrypt pour crypter le mot de passe
-import bcrypt from "bcrypt";
-
-// Importation de la bibliothèque jwt pour créer le cookie/token
-import jwt from "jsonwebtoken";
-
-// Importation des variables d'environement pour le token
-import { ENV } from './../../config.js';
-
-// Fonction pour inscrire l'utilisateur
+// Inscrire l'utilisateur
 const inscrireUtilisateur = async (requete, reponse, next) => {
-    try {
-        const motDePasseHache = await bcrypt.hash(requete.body.mdp, 10); // Hachage du mot de passe
+  try {
+    const motDePasseHache = await bcrypt.hash(requete.body.mdp, 10); // Hachage du mot de passe
 
-        await Utilisateur.create({ ...requete.body, mdp: motDePasseHache, role: 'user' }); // Création d'un nouvelle utilisateur via sequelize
+    await Utilisateur.create( {...requete.body, mdp: motDePasseHache, role: "user"} ); // Création nouvelle utilisateur. On force le role user
 
-        reponse.status(201).json("L'Utilisateur a bien été inscrit !");
-    } catch (erreur) {
-        console.log(erreur);
-        reponse.status(500).json({ error: "Erreur interne lors de la création de l'utilisateur !" });
-    }
-}
+    reponse.status(201).json("L'Utilisateur a bien été inscrit !");
+  } catch (erreur) {
+    console.log(erreur);
+    reponse.status(500).json( {error: "Erreur interne lors de la création de l'utilisateur !" });
+  }
+};
 
-// Fonction pour récupérer tout les utilisateurs
+// Récupérer tout les utilisateurs
 const recupererUtilisateurs = async (requete, reponse, next) => {
-    try {
-        const toutLesUtilisateurs = await Utilisateur.findAll(); // Récupère tous les utilisateurs
+  try {
+    const toutLesUtilisateurs = await Utilisateur.findAll();
+    reponse.status(200).json(toutLesUtilisateurs);
+  } catch (erreur) {
+    console.log(erreur);
+    reponse.status(500).json( {error: "Erreur interne lors de la récupération des utilisateurs !"} );
+  }
+};
 
-        if (toutLesUtilisateurs.length == 0)
-            return reponse.status(200).json("Il n'y a pas encore d'utilisateur !");
-
-        reponse.status(200).json(toutLesUtilisateurs);
-    } catch (erreur) {
-        console.log(erreur);
-        reponse.status(500).json({ error: "Erreur interne lors de la récupération des utilisateurs !" });
-    }
-}
-
-// Fonction pour modifier un utilisateur
+// Modifier un utilisateur
 const modifierUtilisateur = async (requete, reponse, next) => {
-    try {
-        const utilisateurTrouve = await Utilisateur.findByPk(requete.params.id); // Récupère un utilisateur par ID
+  try {
+    const utilisateurTrouve = await Utilisateur.findByPk(requete.params.id); // Récupère un utilisateur par ID dans les paramètres de la requete
 
-        if (!utilisateurTrouve)
-            return reponse.status(404).json("Cette utilisateur n'existe pas !");
+    if (!utilisateurTrouve)
+      return reponse.status(404).json("Cette utilisateur n'existe pas !");
 
-        await utilisateurTrouve.update(requete.body); // Modifie l'utilisateur
-        
-        reponse.status(200).json({
-            message: "L'utilisateur a bien été modifié !",
-            utilisateurTrouve,
-        });
-    } catch (erreur) {
-        console.log(erreur);
-        reponse.status(500).json({ error: "Erreur interne lors de la modification de l'utilisateur !" });
-    }
-}
+    await utilisateurTrouve.update(requete.body); // Modifie l'utilisateur
 
-// Fonction pour supprimer un utilisateur
+    reponse.status(200).json( {message: "L'utilisateur a bien été modifié !", utilisateurTrouve, });
+  } catch (erreur) {
+    console.log(erreur);
+    reponse.status(500).json( {error: "Erreur interne lors de la modification de l'utilisateur !"} );
+  }
+};
+
+// Supprimer un utilisateur
 const supprimerUtilisateur = async (requete, reponse, next) => {
-    try {
-        const utilisateurTrouve = await Utilisateur.findByPk(requete.params.id); // Récupère un utilisateur par ID
+  try {
+    const utilisateurTrouve = await Utilisateur.findByPk(requete.params.id);
 
-        if (!utilisateurTrouve)
-            return utilisateurTrouve.status(404).json("Cette utilisateur n'existe pas !");
+    if (!utilisateurTrouve)
+      return reponse.status(404).json("Cette utilisateur n'existe pas !");
 
-        await utilisateurTrouve.destroy();
-        
-        reponse.status(200).json({
-            message: "L'utilisateur a bien été supprimé !"
-        });
-    } catch (erreur) {
-        console.log(erreur);
-        reponse.status(500).json({ error: "Erreur interne lors de la suppression de l'utilisateur !" });
-    }
-}
+    await utilisateurTrouve.destroy(); // Supprime l'utilisateur
 
-// Fonction pour connecter l'utilisateur
+    reponse.status(200).json( {message: "L'utilisateur a bien été supprimé !"} );
+  } catch (erreur) {
+    console.log(erreur);
+    reponse.status(500).json( {error: "Erreur interne lors de la suppression de l'utilisateur !"} );
+  }
+};
+
+// Connecte l'utilisateur en vérifiant ses identifiants, crée un cookie jwt access_token, puis renvoie l'objet utilisateur
 const connecterUtilisateur = async (requete, reponse, next) => {
-    try {
-        const utilisateurTrouve = await Utilisateur.findOne( { where: { identifiant: requete.body.identifiant } } ); // Recherche de l'utilisateur
-        
-        if (!utilisateurTrouve) // Si l'utilisateur n'est pas trouvé
-            return reponse.status(404).json("L'utilisateur n'a pas été trouvé !");
+  try {
+    const utilisateurTrouve = await Utilisateur.findOne( {where: { identifiant: requete.body.identifiant }} ); // Recherche de l'utilisateur via son identifiant
 
-            // Comparaison du MDP crypté avec la version tapé par l'utilisateur
-            const comparaisonDuMDP = await bcrypt.compare(
-            requete.body.mdp,
-            utilisateurTrouve.mdp
-        );
+    if (!utilisateurTrouve)
+      return reponse.status(404).json("L'utilisateur n'a pas été trouvé !");
 
-        if (!comparaisonDuMDP) // Si la comparaison est fausse
-            return reponse.status(400).json("L'identifiant ou le mot de passe est inccorect !");
+    // Comparaison du MDP crypté avec la version tapé par l'utilisateur
+    const comparaisonDuMDP = await bcrypt.compare( requete.body.mdp, utilisateurTrouve.mdp );
 
-        // Si la comparaison est bonne, création du web token jwt
-        const token = jwt.sign(
-            { id: utilisateurTrouve.id },
-            ENV.TOKEN,
-            { expiresIn: "24h" }
-        );
+    if (!comparaisonDuMDP)
+      return reponse.status(400).json("L'identifiant ou le mot de passe est inccorect !");
 
-        // Renvoi du token dans le cookie et de l'utilisateur en cours
-        reponse
-        .cookie("access_token", token, { httpOnly: true } )
-        .status(200)
-        .json(utilisateurTrouve);
-    } catch (erreur) {
-        console.log(erreur);
-        reponse.status(500).json( { error: "Erreur interne lors de la connection !" } );
-    }
+    
+    const token = jwt.sign( { id: utilisateurTrouve.id }, ENV.TOKEN, { expiresIn: "24h" } ); // Création du web token jwt
+
+    reponse
+      .cookie("access_token", token, { httpOnly: true }) // renvoi du cookier access token jwt
+      .status(200).json(utilisateurTrouve); // renvoi de l'utilisateur en objet
+  } catch (erreur) {
+    console.log(erreur);
+    reponse.status(500).json({ error: "Erreur interne lors de la connection !" });
+  }
+};
+
 // Déconnecte l'utilisateur en supprimant le Cookie
 const deconnecterUtilisateur = async (requete, reponse, next) => {
   try {
@@ -124,14 +101,18 @@ const deconnecterUtilisateur = async (requete, reponse, next) => {
     reponse.status(500).json({ error: "Erreur interne lors de la déconnection !" });
   }
 };
+
+// Récupère l'utilisateur déjà authentifié via requete.user qui a été rempli par verifyToken)
 const recupererUtilisateurCourant = (requete, reponse) => {
-    return reponse.json(requete.user);
-} 
+  return reponse.json(requete.user);
+};
 
 export {
-    connecterUtilisateur,
-    inscrireUtilisateur,
-    recupererUtilisateurs,
-    modifierUtilisateur,
-    supprimerUtilisateur,
+  inscrireUtilisateur,
+  recupererUtilisateurs,
+  modifierUtilisateur,
+  supprimerUtilisateur,
+  connecterUtilisateur,
   deconnecterUtilisateur,
+  recupererUtilisateurCourant,
+};

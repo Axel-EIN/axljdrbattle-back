28baf6afe6ce191a2c596f315e688a1a5ae24135
Axel <axel.ein@gmail.com>
2024-08-06 15:38:53 +0200

    [MIDDLEWARE auth] added complete user in request

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,18 +1,23 @@
 import jwt from 'jsonwebtoken'; // Importation de la librairie pour le JsonWebToken
 import { ENV } from '../../config.js'; // Importation des variables d'environements pour le secret token
 import { createError } from '../utils/error.js'; // Importation de la fonction personnalisable de création d'objet d'erreur
-
+import { Utilisateur } from "../models/index.js";
 const verifyToken = (requete, reponse, next) => {
     const token = requete.cookies.access_token;
 
     if (!token)
-        return next(createError(401, "L'Accès est réfusé !"));
+        return next(createError(401, "L'Accès est réfusé car vous devez être connecté !"));
 
-    jwt.verify(token, ENV.TOKEN, (err, user) => {
-        if (err) {
-            return next(createError(403, { message: "Le token n'est pas valide !", error: err.message }))
-        }
-        requete.user = user; // Si le token est bon, alors l'utilisateur est stocké dans la requête
+    jwt.verify(token, ENV.TOKEN, async (err, user) => {
+        if (err)
+            return next(createError(403, { message: "Le token n'est pas valide !", error: err.message }));
+
+        const userTrouve = await Utilisateur.findByPk(user.id); // Recherche de l'utilisateur dans la base de donnée
+
+        if (!userTrouve)
+            return next(createError(404, { message: "Cet utilisateur n'existe pas !", error: err.message }));
+        
+        requete.user = userTrouve; // Si le token est bon, alors l'utilisateur est stocké dans la requête
         next();
     })
 }

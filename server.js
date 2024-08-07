4da964ef4e3d5d7a77d95e6eebce14062c433a2d
Axel <axel.ein@gmail.com>
2024-08-07 11:04:21 +0200

    [SERVER] reformat and edited comments

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { ENV } from './config.js';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-// import cors from 'cors';
+import express from 'express'; // Module pour gérer les routes
+import cookieParser from 'cookie-parser'; // Module pour pouvoir lire les cookie
+// import cors from 'cors'; // cette manière de faire fonctionner les cors ne semble pas marcher sur Firefox
 
 // Importation des routes
 import routeurUtilisateur from './src/routes/utilisateur.route.js';
@@ -11,7 +11,7 @@ import routeurCombat from './src/routes/combat.route.js';
 // Importation et connexion de la base de données
 import './src/models/index.js'; 
 
-// Lancement d'express
+// Lancement du serveur express
 const app = express();
 
 // Implémentation des middlewares
@@ -19,9 +19,9 @@ app.use(express.json()); // Pour lire les données JSON
 app.use(cookieParser()); // Pour parser les cookies
 // app.use(cors({
 //     origin: "*"
-// })); // Pour gérer la protection des CORS
+// })); // Pour gérer la protection des CORS, cette manière de faire fonctionner les cors ne semble pas marcher sur Firefox
 
-app.use((req, res, next) => {
+app.use((requete, reponse, next) => { // Autre methode pour faire fonctionner les cors
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     return next();
 });
 
-app.use((err, req, res, next) => {
+app.use((err, requete, reponse, next) => { // Middleware pour tracker les erreurs
     const status = err.status || 500;
     const message = err.message || "Quelque chose s'est mal passée !";
     return res.status(status).json({
@@ -39,7 +39,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-// Middleware to route
+// Middleware pour connecter les routes
 app.use("/api/utilisateur", routeurUtilisateur);
 app.use("/api/personnage", routeurPersonnage);
 app.use("/api/combat", routeurCombat);

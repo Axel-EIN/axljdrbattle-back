4645ea3df9e6bc1872b78357aaf4b45afb7eedf4
Axel <axel.ein@gmail.com>
2024-08-07 11:04:53 +0200

    [SERVER] changed res to reponse

--- server.js
+++ server.js
@@ -22,17 +22,17 @@ app.use(cookieParser()); // Pour parser les cookies
 // })); // Pour gérer la protection des CORS, cette manière de faire fonctionner les cors ne semble pas marcher sur Firefox
 
 app.use((requete, reponse, next) => { // Autre methode pour faire fonctionner les cors
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.setHeader("Access-Control-Allow-Credentials", true);
+    reponse.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    reponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    reponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    reponse.setHeader("Access-Control-Allow-Credentials", true);
     return next();
 });
 
 app.use((err, requete, reponse, next) => { // Middleware pour tracker les erreurs
     const status = err.status || 500;
     const message = err.message || "Quelque chose s'est mal passée !";
-    return res.status(status).json({
+    return reponse.status(status).json({
         success: false,
         status,
         message,

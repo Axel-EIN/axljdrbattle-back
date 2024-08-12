6dc451dd858f50879c4076b56b3b6c965117a7de
Axel <axel.ein@gmail.com>
2024-08-12 22:34:22 +0200

    [UTILS] changed export default to specific export for removeFile function

--- src/utils/managefiles.js
+++ src/utils/managefiles.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default function removeFile(publicFilePath) {
+export function removeFile(publicFilePath) {
   fs.unlink(__dirname + '/../..' + '/public/' + publicFilePath, (err) => {
     if (err) {
       console.error(`Erreur lors de la tentative de suppression du fichier : ${err}`);

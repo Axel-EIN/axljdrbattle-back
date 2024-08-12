6e879b051f9179385b3f68dd96a42a443ed70afa
Axel <axel.ein@gmail.com>
2024-08-12 22:42:49 +0200

    [CHARACTER route] implemented retrieveOne and multer and verifyMj middlewares

--- src/routes/personnage.route.js
+++ src/routes/personnage.route.js
@@ -1,17 +1,22 @@
 import express from "express";
+import verifyToken from '../middlewares/auth.js';
+import verifyMj from '../middlewares/mj.js';
+import multer from '../middlewares/multer.js'; // Bibliothèque Middleware qui récupère un fichier image d'un Formulaire et l'upload dans un dossier destination en le renomant
 
 import {
   ajouterPersonnage,
   recupererPersonnages,
+  recupererUnPersonnage,
   modifierPersonnage,
   supprimerPersonnage
 } from "../controllers/personnage.controller.js";
 
 const router = express.Router();
 
-router.post("/ajouter", ajouterPersonnage);
+router.post("/ajouter", verifyToken, verifyMj, multer, ajouterPersonnage);
 router.get("/tous", recupererPersonnages);
-router.put("/modifier/:id", modifierPersonnage);
-router.delete("/supprimer/:id", supprimerPersonnage);
+router.get("/un/:id", recupererUnPersonnage);
+router.put("/modifier/:id", verifyToken, verifyMj, multer, modifierPersonnage);
+router.delete("/supprimer/:id", verifyToken, verifyMj, multer, supprimerPersonnage);
 
 export default router;

d93a9bd5c939c5973f62d89405dd75b71d1536fd
Axel <axel.ein@gmail.com>
2024-11-11 01:55:40 +0100

    [BATTLE route] implemented reinitializeBattle

--- src/routes/combat.route.js
+++ src/routes/combat.route.js
@@ -7,6 +7,7 @@ import {
   modifierCombat,
   supprimerCombat,
   demarrerCombat,
+  recommencerCombat,
 } from "../controllers/combat.controller.js";
 
 const router = express.Router();
@@ -17,5 +18,6 @@ router.post("/ajouter", ajouterCombat);
 router.put("/modifier/:id", modifierCombat);
 router.delete("/supprimer/:id", supprimerCombat);
 router.put("/demarrer/:id", demarrerCombat);
+router.put("/recommencer/:id", recommencerCombat);
 
 export default router;

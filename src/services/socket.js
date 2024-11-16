11349b7ab037e88c14c1855251506a9f878fa03f
Axel <axel.ein@gmail.com>
2024-11-16 17:17:14 +0100

    [SERVICE socket] edited comments

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,19 +1,19 @@
-import app from './../../app.js'; // Importation de de la création du serveur express et de sa config (routes, controlleur)
-import http from "http"; // Importation du module http
+import app from './../../app.js'; // Importation du serveur express crée avec sa config (routes, controlleur)
+import http from "http";
 import { Server } from "socket.io"; // Importation du module server de socketIO
 
-const server = http.createServer(app); // Creation d'un serveur http en utilisant app.js
+const server = http.createServer(app); // Creation d'un serveur http à partir sur serveur express
 
-const io = new Server(server, { // Création d'un serveur Web Socket à partir du serveur http en autorisant les cors sur 5173
+const io = new Server(server, { // Création d'un serveur Web SocketIO en autorisant les cors sur 5173
   cors: {
     origin: "http://localhost:5173",
   },
 });
 
 io.on("connection", (socket) => { // Ecouteur d'évévenement sur la connection au socket
-  console.log(`Le Socket ${socket.id} est bien connecté !`); // Affichage d'un message dans la console lorsque le socket se connect
+  console.log(`Le Socket ${socket.id} est bien connecté !`);
 
-  socket.on("disconnect", () => { // Affichage d'un message dans la console lorsque le socket est déconnecté
+  socket.on("disconnect", () => {
     console.log(`Le Socket ${socket.id} vient de se déconnecter !`);
   });
 
@@ -22,5 +22,4 @@ io.on("connection", (socket) => { // Ecouteur d'évévenement sur la connection 
   });
 });
 
-// Exportation du serveur et de l'instance 'io' pour les utiliser dans d'autres parties de l'application
-export { server, io };
+export { server, io }; // Exportation du serveur http et du serveur io

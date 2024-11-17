2c71b6e200cd05e299322c70fb5570bd13f06d00
Axel <axel.ein@gmail.com>
2024-11-17 05:15:18 +0100

    [MIDDLEWARE gamemaster] renamed file and fixed variables names to english

--- src/middlewares/gamemaster.js
+++ src/middlewares/gamemaster.js
@@ -1,10 +1,10 @@
 import { createError } from '../utils/error.js'; // Importation de la fonction personnalisable de création d'objet d'erreur
 
-const verifyMj = (requete, reponse, next) => {
-    if (requete.user.role != 'mj')
+const verifyGameMaster = (request, response, next) => {
+    if (request.user.role != 'gamemaster')
         return next(createError(401, "L'Accès est restreint aux Maîtres de Jeu !"));
 
     next();
 }
 
-export default verifyMj;
+export default verifyGameMaster;

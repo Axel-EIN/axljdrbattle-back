b47e5c1b6a36358cf731c11eee1c43ed0c7b2e9e
Axel <axel.ein@gmail.com>
2024-11-17 05:14:33 +0100

    [CHARACTER route] fixed variables names to english

--- src/routes/character.route.js
+++ src/routes/character.route.js
@@ -1,22 +1,22 @@
 import express from "express";
 import verifyToken from '../middlewares/auth.js';
-import verifyMj from '../middlewares/mj.js';
+import verifyGameMaster from '../middlewares/gamemaster.js';
 import multer from '../middlewares/multer.js'; // Bibliothèque Middleware qui récupère un fichier image d'un Formulaire et l'upload dans un dossier destination en le renomant
 
 import {
-  ajouterPersonnage,
-  recupererPersonnages,
-  recupererUnPersonnage,
-  modifierPersonnage,
-  supprimerPersonnage
-} from "../controllers/personnage.controller.js";
+  addCharacter,
+  getAllCharacters,
+  getOneCharacter,
+  editCharacter,
+  deleteCharacter
+} from "../controllers/character.controller.js";
 
 const router = express.Router();
 
-router.post("/ajouter", verifyToken, verifyMj, multer, ajouterPersonnage);
-router.get("/tous", recupererPersonnages);
-router.get("/un/:id", recupererUnPersonnage);
-router.put("/modifier/:id", verifyToken, verifyMj, multer, modifierPersonnage);
-router.delete("/supprimer/:id", verifyToken, verifyMj, multer, supprimerPersonnage);
+router.get("/all", getAllCharacters);
+router.get("/one/:id", getOneCharacter);
+router.post("/add", verifyToken, verifyGameMaster, multer, addCharacter);
+router.put("/edit/:id", verifyToken, verifyGameMaster, multer, editCharacter);
+router.delete("/delete/:id", verifyToken, verifyGameMaster, multer, deleteCharacter);
 
 export default router;

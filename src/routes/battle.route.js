9602042e934dd28a3d5b34dd26b171dad3c39b81
Axel <axel.ein@gmail.com>
2024-11-17 05:14:09 +0100

    [BATTLE route] fixed variables names to english

--- src/routes/battle.route.js
+++ src/routes/battle.route.js
@@ -1,27 +1,27 @@
 import express from "express";
 import verifyToken from '../middlewares/auth.js';
 import {
-  recupererCombats,
-  recupererUnCombat,
-  ajouterCombat,
-  modifierCombat,
-  supprimerCombat,
-  demarrerCombat,
-  recommencerCombat,
-  arreterCombat,
-  jouerTour,
-} from "../controllers/combat.controller.js";
+  getAllBattles,
+  getOneBattle,
+  addBattle,
+  editBattle,
+  deleteBattle,
+  startBattle,
+  restartBattle,
+  stopBattle,
+  playTurn,
+} from "../controllers/battle.controller.js";
 
 const router = express.Router();
 
-router.get("/tous", recupererCombats);
-router.get("/un/:id", recupererUnCombat);
-router.post("/ajouter", ajouterCombat);
-router.put("/modifier/:id", modifierCombat);
-router.delete("/supprimer/:id", supprimerCombat);
-router.put("/demarrer/:id", demarrerCombat);
-router.put("/recommencer/:id", recommencerCombat);
-router.put("/arreter/:id", arreterCombat);
-router.put("/jouer/:id", verifyToken, jouerTour);
+router.get("/all", getAllBattles);
+router.get("/one/:id", getOneBattle);
+router.post("/add", addBattle);
+router.put("/edit/:id", editBattle);
+router.delete("/delete/:id", deleteBattle);
+router.put("/start/:id", startBattle);
+router.put("/restart/:id", restartBattle);
+router.put("/stop/:id", stopBattle);
+router.put("/turn/:id", verifyToken, playTurn);
 
 export default router;

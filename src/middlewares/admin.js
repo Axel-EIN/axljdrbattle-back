d4bd2cdb8de31e26b764a8909b495174aa11d3c5
Axel <axel.ein@gmail.com>
2024-11-17 05:15:59 +0100

    [MIDDLEWARE admin] fixed variables names to english

--- src/middlewares/admin.js
+++ src/middlewares/admin.js
@@ -1,9 +1,7 @@
 import { createError } from '../utils/error.js'; // Importation de la fonction personnalisable de création d'objet d'erreur
 
-const verifyAdmin = (requete, reponse, next) => {
-    if (requete.user.role != 'admin')
-        return next(createError(401, "L'Accès est restreint aux Administrateurs !"));
-
+const verifyAdmin = (request, response, next) => {
+    if (request.user.role != 'admin') return next(createError(401, "Désolé ! L'Accès est restreint aux Administrateurs !"));
     next();
 }
 

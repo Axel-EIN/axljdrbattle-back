e1687aa0186705eea78e46491f4f4ca38581afac
Axel <axel.ein@gmail.com>
2024-11-17 05:18:02 +0100

    [MIDDLEWARE auth] fixed variables names to english

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,26 +1,18 @@
 import jwt from 'jsonwebtoken'; // Importation de la librairie pour le JsonWebToken
-import { ENV } from '../../config.js'; // Importation des variables d'environements pour le secret token
+import { ENV } from '../../config.js';
 import { createError } from '../utils/error.js'; // Importation de la fonction personnalisable de création d'objet d'erreur
-import { Utilisateur } from "../models/index.js";
+import { User } from "../models/index.js";
 
-const verifyToken = (requete, reponse, next) => {
-    const token = requete.cookies.access_token;
-
-    if (!token)
-        return next(createError(401, "L'Accès est réfusé car vous devez être connecté !"));
-
+const verifyToken = (request, response, next) => {
+    const token = request.cookies.access_token;
+    if (!token) return next(createError(401, "Le Token JWT n'a pas été trouvé ! L'Accès est réfusé car vous devez être connecté avec un Token !"));
     jwt.verify(token, ENV.TOKEN, async (err, user) => {
-        if (err)
-            return next(createError(403, { message: "Le token n'est pas valide !", error: err.message }));
-
-        const userTrouve = await Utilisateur.findByPk(user.id); // Recherche de l'utilisateur dans la base de donnée
-
-        if (!userTrouve)
-            return next(createError(404, { message: "Cet utilisateur n'existe pas !", error: err.message }));
-        
-        requete.user = userTrouve; // Si le token est bon, alors l'utilisateur est stocké dans la requête
+        if (err) return next(createError(403, { message: "Le token JWT ne semble pas valide !", error: err.message }));
+        const userFound = await User.findByPk(user.id); // Recherche de l'utilisateur dans la base de donnée
+        if (!userFound) return next(createError(404, { message: "Cet utilisateur n'existe pas !", error: err.message }));
+        request.user = userFound; // Si le token est bon, alors l'utilisateur est stocké dans la requête
         next();
-    })
+    });
 }
 
 export default verifyToken;

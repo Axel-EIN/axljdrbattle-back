c9988df0854cb83acf7d6f63eedb1eacc83c4d12
Axel <axel.ein@gmail.com>
2024-12-04 21:33:07 +0100

    [USER controller] included Character to the query for getAllUsers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,7 +11,7 @@ import { removeFile } from "../utils/managefiles.js";
 
 const getAllUsers = async (request, response) => {
   try {
-    const allUsers = await User.findAll();
+    const allUsers = await User.findAll({include: [Character]});
     response.status(200).json(allUsers);
   } catch (error) {
     console.log(error);

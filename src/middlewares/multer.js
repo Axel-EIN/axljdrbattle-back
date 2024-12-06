49a72518f2bcc300f836ce229d242ce7cd5c8190
Axel <axel.ein@gmail.com>
2024-12-06 14:09:03 +0100

    [MIDDLEWARE multer] reworked logic to control filename string if it's an avatar, portrait or illustration

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -2,27 +2,46 @@ import multer from "multer";
 import { checkAndCreateDir } from "../utils/managefiles.js";
 
 const MIME_TYPES = { // association nom et extension de fichier d'images
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-  "image/png": "png",
+    "image/jpg": "jpg",
+    "image/jpeg": "jpg",
+    "image/png": "png",
 };
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    checkAndCreateDir('images/' + file.fieldname + 's/') // Crée le dossier de destination s'il n'existe pas encore
-    callback(null, "public/images/" + file.fieldname + 's' + '/'); // Avec file.fieldname + s on va uploader dans des dossiers spécifiques
-  },
-  filename: (req, file, callback) => { // Crée le nom du fichier
-    const name = file.originalname.split(" ").join("_"); // remplace les espaces par des _
-    const nameDotArray = name.split('.'); // transforme en tableau selon les points
-    nameDotArray.pop(); // enlève la dernière partie du tableau
-    const nameNoExtension = nameDotArray.join("."); // rejoint pour reformer une chaîne
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, nameNoExtension + '_' + Date.now() + "." + extension);
-  },
-});
+const storage = multer.diskStorage(
+    {
+        destination: (req, file, callback) => {
+            const folderName =  file.fieldname + 's/';
+            checkAndCreateDir('images/' + folderName + '/') // Crée le dossier de destination s'il n'existe pas encore
+            callback(null, "public/images/" + folderName + '/'); // Dossier de destination prêt 
+        },
 
-// Execution de multer donc le resultat est stocké dans upload.
+        filename: (req, file, callback) => {
+            const extension = MIME_TYPES[file.mimetype];
+
+            let nameNoExtension = '';
+            switch(file.fieldname){
+                case 'avatar':
+                    nameNoExtension = req.body.login + '_' + 'avatar';
+                    break;
+                case 'portrait':
+                    nameNoExtension = req.body.lastname + '_' + req.body.firstname + '_' + 'portrait';
+                    break;
+                case 'illustration':
+                    nameNoExtension = req.body.lastname + '_' + req.body.firstname + '_' + 'illustration';
+                    break;
+                default:
+                    const name = file.originalname.split(" ").join("_"); // remplace les espaces par des _
+                    const nameDotArray = name.split('.'); // transforme le nome de fichier en tableau d'éléments séparé par des points
+                    nameDotArray.pop(); // enlève la dernière partie du tableau donc la partie avec .extention du nom de fichier
+                    nameNoExtension = nameDotArray.join("."); // rejoint pour reformer une chaîne mais sans l'extention
+            }
+           
+            callback(null, nameNoExtension.toLowerCase() + '_' + Date.now() + "." + extension); // Fichier de destination prêt
+        },
+    }
+);
+
+// Execution de multer dont le resultat est stocké dans la variable upload
 const upload = multer({ storage: storage }).any(); // Initialisation de multer avec le storage définit plus haut
 // Puis utilisation de la methode .single qui va uploader un élément contenu dans le champ "avatar" des données formulaires
 

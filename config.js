f95c4c3f848409ee988e964e8743a696106561ea
Axel <axel.ein@gmail.com>
2024-12-17 21:22:53 +0100

    [CONFIG] added mail secrets

--- config.js
+++ config.js
@@ -11,4 +11,10 @@ export const ENV = { // Exporte la constante ENV avec les infos
     DB_NAME: process.env.DB_NAME,
     DB_PASSWORD: process.env.DB_PASSWORD,
     WEB_APP_URL: process.env.WEB_APP_URL,
+    MAIL_FROM: process.env.MAIL_FROM,
+    MAIL_HOST: process.env.MAIL_HOST,
+    MAIL_PORT: process.env.MAIL_PORT,
+    MAIL_USER: process.env.MAIL_USER,
+    MAIL_PASSWORD: process.env.MAIL_PASSWORD,
+    MAIL_TO: process.env.MAIL_TO,
 };

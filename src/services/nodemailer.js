6f60f8f1d8fdccfefa88dd317ff3b06330a84df3
Axel <axel.ein@gmail.com>
2024-12-18 03:41:51 +0100

    [SERVICE nodemailer] changed the sender to mailgun

--- src/services/nodemailer.js
+++ src/services/nodemailer.js
@@ -1,31 +1,42 @@
 import nodemailer from "nodemailer";
+import nodemailerMailgunTransport from "nodemailer-mailgun-transport";
 import { ENV } from './../../config.js';
 
-const transport = nodemailer.createTransport({
-    host: ENV.MAIL_HOST,
-    port: ENV.MAIL_PORT,
-    secure: true,
+const mailgunAuth = {
     auth: {
-        user: ENV.MAIL_USER,
-        pass: ENV.MAIL_PASSWORD,
-    },
-});
+        api_key: ENV.API_KEY_MAILGUN,
+        domain: ENV.DOMAIN_MAILGUN,
+    }
+};
+
+
+
+// const transport = nodemailer.createTransport({
+//     host: ENV.MAIL_HOST,
+//     port: ENV.MAIL_PORT,
+//     secure: true,
+//     auth: {
+//         user: ENV.MAIL_USER,
+//         pass: ENV.MAIL_PASSWORD,
+//     },
+// });
 
 export let sendMailToUser = async (user, verifyEmailToken) => {
     try {
       const verifyLinkToken = `<a href="${ENV.WEB_APP_URL}/verification/${verifyEmailToken}">${verifyEmailToken}</a>`;
   
       if (ENV.NODE_ENV != 'test') {
-          await transport.sendMail({
-            from: ENV.MAIL_FROM,
-            to: user.email,
-            subject: "Validation du Mail",
-            text: `Bonjour, ${user.firstname}, ceci est un mail pour valider votre e-mail`, // Au cas où l'html ne passe pas
-            html: `
-                      <p>Veuillez cliquez sur ce lien.</p>
-                      ${verifyLinkToken}
-                  `,
-          });
+            const smtpTransport = nodemailer.createTransport(nodemailerMailgunTransport(mailgunAuth));
+            await smtpTransport.sendMail({
+                from: ENV.MAIL_FROM,
+                to: user.email,
+                subject: "Validation du Mail",
+                text: `Bonjour, ${user.firstname}, ceci est un mail pour valider votre e-mail`, // Au cas où l'html ne passe pas
+                html: `
+                        <p>Veuillez cliquez sur ce lien.</p>
+                        ${verifyLinkToken}
+                    `,
+            });
       }
 
     } catch (erreur) {

4571987ada1774d8138ba5517ed9140f4baaa356
Axel <axel.ein@gmail.com>
2024-12-18 02:02:24 +0100

    [MIDDLEWARE auth] fixed err.message malfunctioning

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,14 +9,14 @@ const verifyToken = (request, response, next) => {
     const token = request.cookies.access_token;
     if (!token) return next(createError(401, "Le Token JWT n'a pas été trouvé ! L'Accès est réfusé car vous devez être connecté avec un Token !"));
     jwt.verify(token, ENV.TOKEN, async (err, user) => {
-        if (err) return next(createError(403, { message: "Le token JWT ne semble pas valide !", error: err.message }));
+        if (err) return next(createError(403, { message: "Le token JWT ne semble pas valide !", error: err }));
         // const userFound = await User.findByPk(user.id); // Recherche de l'utilisateur dans la base de donnée
 
         const userFound = await User.findByPk(user.id, { include: [
             {model: Character, include: [Participation]}
         ]});
 
-        if (!userFound) return next(createError(404, { message: "Cet utilisateur n'existe pas !", error: err.message }));
+        if (!userFound) return next(createError(404, { message: "Cet utilisateur n'existe pas !", error: err }));
 
         request.user = userFound; // Si le token est bon, alors l'utilisateur est stocké dans la requête
         next();
